dc91ab6cce9a27095e1191092e0d5150b43d477b
WaterKevin123 <kevinsacancela5@gmail.com>
2025-11-06 16:55:55 -0500

    Fix AppRoutes component declaration and basename

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import routes from './config';
 
-export const AppRoutes: React.FC = () => {
+const AppRoutes: React.FC = () => {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={import.meta.env.BASE_URL || '/'}>
       <Routes>
         {routes.map((r: any, i: number) => (
           <Route key={i} path={r.path} element={r.element} />
